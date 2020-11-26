1f7fe45e791edbdc1fd6e4ba39d82f82c09b6951
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-11-26 17:06:45 -0300

    Config home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Logo from './components/template/Logo'
+import Nav from './components/template/Nav'
+import Main from './components/template/Main'
+import Footer from './components/template/Footer'
 
-export default App;
+export default props =>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Main />
+            <Footer />
+        </div>
